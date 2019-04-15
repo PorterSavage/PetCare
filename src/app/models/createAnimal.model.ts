a2bcf0e76f6dc224bdf12a775fd1d319d154ae47
Porter Savage <theportersavage@gmail.com>
2019-04-15 13:37:08 -0700

    Created an Animal object with all attributes that the form will hold

---
+++ src/app/models/createAnimal.model.ts
@@ -0,0 +1,3 @@
+export class Animal {
+  constructor(public ownerFirstName: string, public ownerLastName: string, public animalName: string, public species: string, public gender: string, public stay: number, public dropOff: Date) { }
+}
