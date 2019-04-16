9359f30e912317b59db252c152d8cbb054a69097
Porter Savage <theportersavage@gmail.com>
2019-04-16 11:32:07 -0700

    Added firebase and the ability to put data from the input fields into firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimalService } from '../animal.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [AnimalService]
 })
 export class HomeComponent implements OnInit {
+  animals: FirebaseListObservable<any[]>
 
-  constructor() { }
+  constructor(private animalService: AnimalService) { }
 
   ngOnInit() {
+    this.animals = this.animalService.getAnimals();
   }
-
 }
