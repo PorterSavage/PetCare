0184be4fc5841fd345a5fb3994bb136f4e66304d
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-17 11:08:06 -0700

    user authentication working feelling smart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -37,7 +36,8 @@ export const firebaseConfig = {
     AppRoutingModule,
     FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { AnimalService } from '../animal.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { AuthenticationService } from '../authentication.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  providers: [AnimalService,AuthenticationService]
 })
-export class NavComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NavComponent {
+    user;
+    private isLoggedIn: Boolean;
+    private userName: String;
+  constructor(public authService: AuthenticationService) {
+    this.authService.user.subscribe(user =>  {
+      if (user == null) {
+  this.isLoggedIn = false;
+} else {
+  this.isLoggedIn = true;
+  this.userName = user.displayName;
+}
+    });
   }
 
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
