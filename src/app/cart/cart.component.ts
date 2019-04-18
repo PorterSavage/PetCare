13d2f6a72a55f43f676725f81a74f40c9b9b9100
ThearyIm <thearyim@gmail.com>
2019-04-17 23:36:21 -0700

    Update home, cart and navbar html. Add the jquery to show and hide properly

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimalService } from '../animal.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
+  providers: [AnimalService,AuthenticationService]
 })
 export class CartComponent implements OnInit {
+  animals: FirebaseListObservable<any[]>
 
-  constructor() { }
+  constructor(private animalService: AnimalService) { }
 
   ngOnInit() {
+    this.animals = this.animalService.getAnimals();
   }
 
 }
